9fefaa9057a2c8e911bb4d3f79453ae6884f02da
GitHub Action <action@github.com>
2024-03-11 06:15:37 +0000

    Automatically regenerate server stubs

--- src-gen/swagger_server/__main__.py
+++ src-gen/swagger_server/__main__.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
 import connexion
-from flask import send_from_directory
-
 from swagger_server import encoder
 from flask_jwt_extended import JWTManager
 
@@ -12,12 +10,13 @@ def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'kb-indexer API'}, pythonic_params=True)
-
+    
     app.app.config['JWT_SECRET_KEY'] = "super-secret"  # Change this!
     jwt = JWTManager(app.app)
 
     app.app.register_blueprint(main_bp)
     app.run(port=8080)
+
 
 if __name__ == '__main__':
     main()
